4b55dbf0bf1d4541b6481720b49657b272e0b371
Brenden <17239750+innateessence@users.noreply.github.com>
2024-08-08 18:47:26 -0700

    Delete zsh/docker-functions.zsh

--- zsh/docker-functions.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-function docker_rmi_dangling(){
-    local DANGLING_IMAGES=($(docker images -a --filter=dangling=true -q | tr '\n' ' '))  # An array of dangling image ids
-    for image in "${DANGLING_IMAGES[@]}"; do
-        echo "[*] Removing image $image"
-        docker rmi "$image"
-    done
-}
