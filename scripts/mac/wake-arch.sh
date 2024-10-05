312556fc80d7e55fb97ed7a76b85ad1abf5e0071
JackofSpades707 <jackofspades707@hotmail.com>
2024-10-04 20:21:10 -0700

    update dotfiles

--- scripts/mac/wake-arch.sh
+++ scripts/mac/wake-arch.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 function get_local_ip(){
-  # NOTE: Theoretically works everywhere except native windows
   ifconfig | grep 'inet' | grep -v 'inet6' | grep -v '127.0.0.1' | awk '{print $2}' | head -n 1 | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'
 }
 
