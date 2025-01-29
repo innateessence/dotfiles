da53127e3b88e2f2a2d4faed8203ed5bfbe12c3d
innateessence <jackofspades707@hotmail.com>
2025-01-29 09:07:00 -0800

    update dotfiles

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -143,7 +143,7 @@ if __is_wsl; then
 fi
 
 if __is_ubuntu; then
-    alias upgrade="sudo apt update && sudo apt upgrade -y && apt autoremove -y"
+    alias upgrade="sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y"
 fi
 
 # Unset aliases if the expected dependency doesn't exist in path
