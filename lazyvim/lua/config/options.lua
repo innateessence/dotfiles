d545bb4c34ef6523cd6ce3a62f2b058120f578f0
JackofSpades707 <jackofspades707@hotmail.com>
2025-05-18 20:37:25 -0700

    update dotfiles

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -2,5 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.autoformat = false
-vim.opt.relativenumber = false
+vim.g.autoformat = false -- configure auto code format on write/save
+vim.opt.relativenumber = false -- configure relative line numbers
+vim.opt.expandtab = true -- convert tabs into spaces <Ctrl+V> <TAB> to send real tab.
+vim.opt.whichwrap = "<,>,[,]" -- Configure left/right arrow keys to move cursor to the prev/next line
